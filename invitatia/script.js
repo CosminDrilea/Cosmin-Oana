5357593ae4cd201600cfc71ae0d93a5d62695a7a
CosminDrilea <117672814+CosminDrilea@users.noreply.github.com>
2026-01-20 21:26:12 +0200

    Update script.js

--- invitatia/script.js
+++ invitatia/script.js
@@ -1,1 +1,16 @@
+function openEnvelope(){
+  document.querySelector('.envelope').classList.add('open');
+  setTimeout(()=>{
+    document.getElementById('envelope').style.display='none';
+    document.getElementById('content').style.display='block';
+  },1000);
+}
 
+// COUNTDOWN
+const target = new Date("2026-09-05T00:00:00").getTime();
+setInterval(()=>{
+  const now = new Date().getTime();
+  const d = target - now;
+  document.getElementById("countdown").innerHTML =
+    Math.floor(d/(1000*60*60*24)) + " zile";
+},1000);
